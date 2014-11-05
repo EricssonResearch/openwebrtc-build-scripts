789a6ecdb58c366b9e0fbee97b90190ce49186e9
Sebastian Dröge <sebastian@centricular.com>
2014-11-05 10:09:02 +0100

    libffi: Use autotools build system on iOS too
    Fixes build with the iOS simulator and removes some more special casing.
    
    https://github.com/EricssonResearch/openwebrtc/issues/70

--- dependencies/build-libffi.sh
+++ dependencies/build-libffi.sh
@@ -60,63 +60,34 @@ build() {
     pushd ${builddir} > /dev/null
     rm -fr ./$BUILD_DIR && cp -R ${home}/$BUILD_DIR/* .
 
-    if [[ ${target_triple} == "arm-apple-darwin10" ]]; then
-        # need to apply specific patch to sources to build for ios
-        patch -p1 < ${home}/libffi-${LIBFFI_VERSION}_ios_build.patch
+    if [[ ${target_triple} == "arm-linux-androideabi" ]]; then
+        local platform_configure_flags="--host=${target_triple}"
+        export CFLAGS="$PLATFORM_CFLAGS -DFFI_MMAP_EXEC_WRIT=1"
+    elif [[ ${target_triple} == "arm-apple-darwin10" ]]; then
+        local platform_configure_flags="--host=${target_triple}"
+	export CC="$CC -no-integrated-as"
+	export CCAS="$CC -no-integrated-as"
+        export CFLAGS=$PLATFORM_CFLAGS
+    elif [[ ${target_triple} == "i386-apple-darwin10" ]]; then
+        local platform_configure_flags="--host=${target_triple}"
+	export CC="$CC -no-integrated-as"
+	export CCAS="$CC -no-integrated-as"
+        export CFLAGS=$PLATFORM_CFLAGS
+    else
+        local platform_configure_flags=""
+        export CFLAGS=$PLATFORM_CFLAGS
+    fi
 
-        mkdir -p build_simulator
-        python generate-ios-source-and-headers.py
-        pushd build_ios > /dev/null
-
-        ( # subshell to avoid poluting the shell
-
-            export CFLAGS=$PLATFORM_CFLAGS
-            make && make install
+    ( # subshell to avoid polution
+        ./configure \
+            ${platform_configure_flags} \
+            --prefix=${installdir} \
+            --enable-static \
+            --disable-shared \
+            && make && make install
         )
 
-        local rval=$?
-        popd > /dev/null
-
-        # current workaround is to copy installed file to correct place instead of patching libffi python script.
-        cp -R ${builddir}/armv7-ios/* ${installdir}
-
-    elif [[ ${target_triple} == "i386-apple-darwin10" ]]; then
-
-        patch -p1 < ${home}/libffi-${LIBFFI_VERSION}_ios_build.patch
-
-        mkdir -p build_ios # disables build for device
-        python generate-ios-source-and-headers.py
-        pushd build_simulator > /dev/null
-
-        (
-            export CFLAGS=$PLATFORM_CFLAGS
-            make && make install
-            )
-        local rval=$?
-        popd > /dev/null
-
-        # current workaround is to copy installed file to correct place instead of patching libffi python script.
-        cp -R ${builddir}/i386-ios/* ${installdir}
-    else
-        if [[ ${target_triple} == "arm-linux-androideabi" ]]; then
-            local platform_configure_flags="--host=${target_triple}"
-            export CFLAGS="$PLATFORM_CFLAGS -DFFI_MMAP_EXEC_WRIT=1"
-        else
-            local platform_configure_flags=""
-            export CFLAGS=$PLATFORM_CFLAGS
-        fi
-
-        ( # subshell to avoid polution
-            ./configure \
-                ${platform_configure_flags} \
-                --prefix=${installdir} \
-                --enable-static \
-                --disable-shared \
-                && make && make install
-            )
-
-        local rval=$?
-    fi
+    local rval=$?
 
     popd > /dev/null
     return $rval
